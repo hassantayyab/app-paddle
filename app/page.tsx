11fc9913ca7d9334e845d2fbff0c181605a88e02
Hassan Tayyab <hassan@secfix.com>
2024-10-18 22:52:37 +0200

    Remove projects section

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,6 @@
 import About from './components/About/About';
 import Contact from './components/Contact/Contact';
 import Hero from './components/Hero/Hero';
-import Projects from './components/Projects/Projects';
 import Layout from './layout';
 
 const Home: React.FC = () => {
@@ -9,7 +8,7 @@ const Home: React.FC = () => {
     <Layout>
       <Hero />
       <About />
-      <Projects />
+      {/* <Projects /> */}
       <Contact />
     </Layout>
   );
